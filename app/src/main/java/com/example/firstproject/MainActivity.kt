69289793f0e3a697797dc78af5e73c43a921f8d3
Vikash Kumar Chauhan <vikch@Vikashs-MacBook-Air.local>
2021-05-01 19:23:20 +0530

    Game functional

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -4,29 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() , View.OnClickListener{
     var player = true
     var turn_count = 0
     lateinit var board : Array<Array<Button>>
     var boardStatus = Array(3){IntArray(3)}
-
+    lateinit var tv1 : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var btn1:Button = findViewById<Button>(R.id.btn1)
+        var btn2:Button = findViewById<Button>(R.id.btn2)
+        var btn3:Button = findViewById<Button>(R.id.btn3)
+        var btn4:Button = findViewById<Button>(R.id.btn4)
+        var btn5:Button = findViewById<Button>(R.id.btn5)
+        var btn6:Button = findViewById<Button>(R.id.btn6)
+        var btn7:Button = findViewById<Button>(R.id.btn7)
+        var btn8:Button = findViewById<Button>(R.id.btn8)
+        var btn9:Button = findViewById<Button>(R.id.btn9)
 
-        var btn1 = findViewById<Button>(R.id.btn1)
-        var btn2 = findViewById<Button>(R.id.btn2)
-        var btn3 = findViewById<Button>(R.id.btn3)
-        var btn4 = findViewById<Button>(R.id.btn4)
-        var btn5 = findViewById<Button>(R.id.btn5)
-        var btn6 = findViewById<Button>(R.id.btn6)
-        var btn7 = findViewById<Button>(R.id.btn7)
-        var btn8 = findViewById<Button>(R.id.btn8)
-        var btn9 = findViewById<Button>(R.id.btn9)
+        tv1 = findViewById(R.id.tv1)
 
-        board = arrayOf(
+        this.board = arrayOf(
                 arrayOf(btn1, btn2, btn3),
                 arrayOf(btn4, btn5, btn6),
                 arrayOf(btn7, btn8, btn9)
@@ -37,18 +39,20 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
                 button.setOnClickListener(this)
             }
         }
-        intialiseBoard()
+        initializeBoard()
 
         var resetbtn: Button = findViewById(R.id.resetbtn)
-        resetbtn.setOnClickListener({
-            intialiseBoard()
+        resetbtn.setOnClickListener {
+            initializeBoard()
             player = true
             turn_count = 0
-        })
+            tv1.setText("Player X turn")
+        }
+        tv1.setText("Player X turn")
 
     }
 
-    private fun intialiseBoard() {
+    private fun initializeBoard() {
         for(i in 0..2){
             for (j in 0..2){
                 boardStatus[i][j] = -1
@@ -61,38 +65,88 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     override fun onClick(v: View) {
         when(v.id) {
             R.id.btn1 -> {
-                updatestatus(0 , 0 , player)
-
+                updateStatus(0 , 0 , player)
             }
             R.id.btn2 -> {
-                updatestatus(0 , 1 , player)
+                updateStatus(0 , 1 , player)
             }
             R.id.btn3 -> {
-                updatestatus(0 , 2 , player)
+                updateStatus(0 , 2 , player)
             }
             R.id.btn4 -> {
-                updatestatus(1 , 0 , player)
+                updateStatus(1 , 0 , player)
             }
             R.id.btn5 -> {
-                updatestatus(1 , 1 , player)
+                updateStatus(1 , 1 , player)
             }
             R.id.btn6 -> {
-                updatestatus(1 , 2 , player)
+                updateStatus(1 , 2 , player)
             }
             R.id.btn7 -> {
-                updatestatus(2 , 0 , player)
+                updateStatus(2 , 0 , player)
             }
             R.id.btn8 -> {
-                updatestatus(2 , 1 , player)
+                updateStatus(2 , 1 , player)
             }
             R.id.btn9 -> {
-                updatestatus(2 , 2 , player)
+                updateStatus(2 , 2 , player)
             }
         }
+        turn_count++
         player = if (player) false else true
+
+        if(player == true){
+            disptv("Player X turn")
+        }else {
+            disptv("Player 0 turn")
+        }
+        if(turn_count == 9) {
+            disptv("Draw")
+        }
+        someoneWon()
     }
 
-    private fun updatestatus(row: Int, col: Int, player: Boolean) {
+    private fun disptv( s: String) {
+
+        tv1.setText(s)
+    }
+
+    private fun someoneWon() {
+        var winner  = -1
+        for (i in 0..2){
+            if(boardStatus[i][0] == boardStatus[i][1] && boardStatus[i][0] == boardStatus[i][2]){
+                winner = boardStatus[i][0]
+                break
+            }
+            if(boardStatus[0][i] == boardStatus[1][i] && boardStatus[0][i] == boardStatus[2][i]){
+               winner = boardStatus[0][i]
+                break
+            }
+            }
+
+        if(winner == -1 && boardStatus[0][0] == boardStatus[1][1] && boardStatus[0][0] == boardStatus[2][2]){
+            winner = boardStatus[0][0]
+        }
+        if(winner == -1 && boardStatus[0][2] == boardStatus[1][1] && boardStatus[0][2] == boardStatus[2][0]){
+            winner = boardStatus[0][2]
+        }
+        if(winner != -1){
+            disableClicks()
+            val v = if(winner == 1) "X" else "O"
+            disptv("Player $v  Wins")
+        }
+    }
+
+
+    private fun disableClicks() {
+        for(x in board){
+            for(button in x){
+                button.isEnabled = false
+            }
+        }
+    }
+
+    private fun updateStatus(row: Int, col: Int, player: Boolean) {
         val x = if(player)  "X" else "O"
         val y = if(player) 1 else 0
         board[row][col].apply {
