5c101199c667d373e0cf3db999b0e35ebada9146
Vikash Kumar Chauhan <vikch@Vikashs-MacBook-Air.local>
2021-05-01 10:28:56 +0530

    buttons functional

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -6,7 +6,11 @@ import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() , View.OnClickListener{
+    var player = true
+    var turn_count = 0
     lateinit var board : Array<Array<Button>>
+    var boardStatus = Array(3){IntArray(3)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,42 +37,69 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
                 button.setOnClickListener(this)
             }
         }
+        intialiseBoard()
+
         var resetbtn: Button = findViewById(R.id.resetbtn)
         resetbtn.setOnClickListener({
-
+            intialiseBoard()
+            player = true
+            turn_count = 0
         })
 
+    }
+
+    private fun intialiseBoard() {
+        for(i in 0..2){
+            for (j in 0..2){
+                boardStatus[i][j] = -1
+                board[i][j].isEnabled = true
+                board[i][j].setText("")
+            }
+        }
     }
 
     override fun onClick(v: View) {
         when(v.id) {
             R.id.btn1 -> {
+                updatestatus(0 , 0 , player)
 
             }
             R.id.btn2 -> {
-
+                updatestatus(0 , 1 , player)
             }
             R.id.btn3 -> {
-
+                updatestatus(0 , 2 , player)
             }
             R.id.btn4 -> {
-
+                updatestatus(1 , 0 , player)
             }
             R.id.btn5 -> {
-
+                updatestatus(1 , 1 , player)
             }
             R.id.btn6 -> {
-
+                updatestatus(1 , 2 , player)
             }
             R.id.btn7 -> {
-
+                updatestatus(2 , 0 , player)
             }
             R.id.btn8 -> {
-
+                updatestatus(2 , 1 , player)
             }
             R.id.btn9 -> {
-
+                updatestatus(2 , 2 , player)
             }
         }
+        player = if (player) false else true
+    }
+
+    private fun updatestatus(row: Int, col: Int, player: Boolean) {
+        val x = if(player)  "X" else "O"
+        val y = if(player) 1 else 0
+        board[row][col].apply {
+            isEnabled = false
+            setText(x)
+        }
+        boardStatus[row][col] = y
+
     }
 }
